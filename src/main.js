1ef609816450706017e7a1cfdc3b046960cd3a7c
newSmile726 <1432072169@qq.com>
2022-07-31 10:08:20 +0800

    完成了项目初始化配置

--- src/main.js
+++ src/main.js
@@ -15,14 +15,7 @@ import router from './router'
 import '@/icons' // icon
 import '@/permission' // permission control
 
-/**
- * If you don't want to use mock-server
- * you want to use MockJs for mock api
- * you can execute: mockXHR()
- *
- * Currently MockJs will be used in the production environment,
- * please remove it before going online ! ! !
- */
+//假数据
 if (process.env.NODE_ENV === 'production') {
   const { mockXHR } = require('../mock')
   mockXHR()
@@ -39,5 +32,5 @@ new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 })
